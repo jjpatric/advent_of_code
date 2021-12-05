5182fceb3211d2df3cfcf2fc50965ec0cb7ee3d7
Josh Patrick <jjpatric@ualberta.ca>
2021-12-04 22:16:12 -0700

    Day3 Part2 Complete

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,6 +1,7 @@
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
+use array_tool::vec::Intersect;
 
 fn main() {
     let filename: &str = "input.txt";
@@ -42,9 +43,7 @@ fn day3_part1(filename: &str) -> i32{
         else if bit >= 0 {
             epsilon_rate += 1;
         }
-    }
-    println!("{}", gamma_rate);
-    println!("{}", epsilon_rate);    
+    }   
     
     let r: i32 = gamma_rate * epsilon_rate;
     return r;
@@ -52,16 +51,110 @@ fn day3_part1(filename: &str) -> i32{
 
 fn day3_part2(filename: &str) -> i32{
     // Variables
+    let mut bit_balance: Vec<i32> = Vec::new();
+    let mut all_entries: Vec<String> = Vec::new();
 
     if let Ok(lines) = read_lines(filename) {
         for line in lines {
-            if let Ok(_ip) = line {
+            if let Ok(ip) = line {
                 // Do sumthin
+                all_entries.push(ip.clone());
+                if bit_balance.len() == 0 { bit_balance = vec![0; ip.len()]; }
+                for (i, c) in ip.chars().enumerate() {
+                    let bit: u32 = c.to_digit(2).unwrap();
+                    match bit {
+                        0 => bit_balance[i] -= 1,
+                        1 => bit_balance[i] += 1,
+                        _ => println!("invalid bit"),
+                    }
+                }
             }
         }
     }
-    let r: i32 = 2;
+
+    let mut co2: i32 = 0;
+    let mut oxy: i32 = 0;
+    let mut co2_filter_bit: i32 = 0;
+    let mut oxy_filter_bit: i32 = 0;
+    let mut co2_set:Vec<String> = all_entries.clone();
+    let mut oxy_set:Vec<String> = all_entries.clone();
+    let mut co2_bit_balance: Vec<i32> = bit_balance.clone();
+    let mut oxy_bit_balance: Vec<i32> = bit_balance.clone();
+
+    println!("bit balance: {:?}", bit_balance);
+    for i in 0..bit_balance.len() {
+        let mut co2_matching_entries:Vec<String> = Vec::new();
+        let mut oxy_matching_entries:Vec<String> = Vec::new();
+        let mut co2_bit: i32;
+        if i < co2_bit_balance.len() { co2_bit = co2_bit_balance[i]; }
+        else { co2_bit = 0 }
+        let mut oxy_bit: i32;
+        if i < oxy_bit_balance.len() { oxy_bit = oxy_bit_balance[i]; }
+        else { oxy_bit = 0 }
+
+        if co2_bit < 0 { co2_filter_bit = 1 }
+        else { co2_filter_bit = 0 }
+
+        if oxy_bit < 0 { oxy_filter_bit = 0 }
+        else { oxy_filter_bit = 1 }
+        println!("filter bits co2: {:?} oxy: {:?}", co2_filter_bit, oxy_filter_bit);
+
+
+        for entry in all_entries.iter() {
+            if entry.chars().nth(i).unwrap().to_digit(2).unwrap() == co2_filter_bit.try_into().unwrap() {
+                co2_matching_entries.push(entry.to_string());
+            }
+            if entry.chars().nth(i).unwrap().to_digit(2).unwrap() == oxy_filter_bit.try_into().unwrap() {
+                oxy_matching_entries.push(entry.to_string());
+            }
+        }
+        co2_set = co2_set.intersect(co2_matching_entries.clone());
+        co2_bit_balance = get_bit_balance(co2_set.clone());
+        oxy_set = oxy_set.intersect(oxy_matching_entries.clone());
+        oxy_bit_balance = get_bit_balance(oxy_set.clone());
+
+        println!("Matching Sets co2: {:?} oxy: {:?}", co2_set, oxy_set);
+        if co2_set.len() == 1 {
+            let winning_entry = co2_set.pop().unwrap();
+            println!("co2 winning entry: {}", winning_entry);
+            co2 = string_to_binary(winning_entry.clone());
+        }
+        if oxy_set.len() == 1 {
+            let winning_entry = oxy_set.pop().unwrap();
+            println!("oxy winning entry: {}", winning_entry);
+            oxy = string_to_binary(winning_entry.clone());
+        }
+    }  
+    
+    let r: i32 = co2 * oxy;
     return r;
+}
+
+fn get_bit_balance(v: Vec<String>) -> Vec<i32> {
+    let mut bit_balance: Vec<i32> = Vec::new();
+    for s in v {
+        for (i, c) in s.chars().enumerate() {
+            if bit_balance.len() == 0 { bit_balance = vec![0; s.len()]; }
+            let bit: u32 = c.to_digit(2).unwrap();
+            match bit {
+                0 => bit_balance[i] -= 1,
+                1 => bit_balance[i] += 1,
+                _ => println!("invalid bit"),
+            }
+        }
+    }
+    bit_balance
+}
+
+fn string_to_binary(s: String) -> i32 {
+    let mut value = 0;
+    for bit in s.chars() {
+        value *= 2;
+        if bit == '1' {
+            value += 1;
+        }
+    } 
+    value
 }
 
 
