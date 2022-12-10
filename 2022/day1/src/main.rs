dac289037cf56a04fc4fa462dc4e1ae65623097e
Josh Patrick <jjpatric@ualberta.ca>
2022-12-10 11:09:28 -0700

    Day1 Part2

--- 2022/day1/src/main.rs
+++ 2022/day1/src/main.rs
@@ -5,7 +5,7 @@ use std::path::Path;
 fn main() {
     let filename: &str = "input.txt";
 
-    let mut max_cals: i32 = 0;
+    let mut top_3: Vec<i32> = vec![0, 0, 0];
     let mut cals: i32 = 0;
     let mut line_num = 0;
     
@@ -17,8 +17,10 @@ fn main() {
                 match line.parse::<i32>() {
                     Ok(snack_cals) => cals += snack_cals,
                     Err(_) => {
-                        if cals > max_cals {
-                            max_cals = cals;
+                        let min_top_3 = *top_3.iter().min().unwrap();
+                        if cals > min_top_3 {
+                            let index_of_min: usize = get_min_val_index(&top_3);
+                            top_3[index_of_min] = cals;
                         }
                         cals = 0;
                     }
@@ -27,9 +29,17 @@ fn main() {
             }
         }
     }
-    println!("\nAnswer: {}", max_cals);
+    println!("\nAnswer: {}", top_3.iter().sum::<i32>());
 }
 
+fn get_min_val_index(nets: &Vec<i32>) -> usize{
+    let index_of_min: Option<usize> = nets
+        .iter()
+        .enumerate()
+        .min_by(|(_, a), (_, b)| a.cmp(b))
+        .map(|(index, _)| index);
+    return index_of_min.unwrap();
+}
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
 where P: AsRef<Path>, {
